09e25b8daad473f4e317575882ba20e7eb76ac19
Erekle Papuashvili <erekle.papuashvili@gmail.com>
2024-11-20 16:08:34 -0500

    function connects to global scope

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Write your solution in this file!
+
+var customerName = 'bob';
+var bestCustomer; 
+
+() => {customerName}
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+};
+
+
+
+function setBestCustomer () {
+    bestCustomer = "not bob"
+}
+
+function overwriteBestCustomer () {
+    bestCustomer = "maybe bob"
+}
+
+const leastFavoriteCustomer = "greg"
+function changeLeastFavoriteCustomer () {
+    leastFavoriteCustomer = "bob"
+}
